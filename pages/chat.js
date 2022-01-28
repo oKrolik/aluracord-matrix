1f90f21cdbed28eb1265dd384cad58554ae6f781
oKrolik <gcoelho2002@gmail.com>
2022-01-28 19:21:38 +0000

    changed the chat size

--- pages/chat.js
+++ pages/chat.js
@@ -57,7 +57,7 @@ export default function ChatPage() {
                     borderRadius: '4px',
                     // backgroundColor: appConfig.theme.colors.neutrals[700],
                     backgroundColor: 'rgba(33,41,49,0.9)',
-                    height: '70%',
+                    height: '60%',
                     maxWidth: '85%',
                     maxHeight: '95vh',
                     padding: '8px',
